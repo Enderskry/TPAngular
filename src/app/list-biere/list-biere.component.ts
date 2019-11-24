1e0a909571011af541c7e9875c0fde0083c186f8
Bruno_ANDRIEU <andrieu.bruno@gmail.com>
2019-11-24 18:15:10 +0100

    debug anim

--- src/app/list-biere/list-biere.component.ts
+++ src/app/list-biere/list-biere.component.ts
@@ -13,7 +13,7 @@ import { FormControl } from '@angular/forms';
         query(':enter', style({ opacity: 0 }), { optional: true }),
 
         query(':enter', stagger('300ms', [
-          animate('1s ease-in', keyframes([
+          animate('.5s ease-in', keyframes([
             style({ opacity: 0, transform: 'translateY(-75%)', offset: 0 }),
             style({ opacity: .5, transform: 'translateY(35px)', offset: .3 }),
             style({ opacity: 1, transform: 'translateY(0px)', offset: 1 })
