71ebcfa8e27b4fe94ce40b3c51c3c8f3c5c2b1a4
Bruno_ANDRIEU <andrieu.bruno@gmail.com>
2019-11-24 22:20:58 +0100

    merge Validators Romain

--- src/app/biere/biere.component.ts
+++ src/app/biere/biere.component.ts
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import { Component, OnInit, Input } from '@angular/core';
-import Biere from '../biere';
-
-@Component({
-  selector: 'app-biere',
-  templateUrl: './biere.component.html',
-  styleUrls: ['./biere.component.css']
-})
-export class BiereComponent implements OnInit {
-  
-  @Input()
-  b: Biere
-
-  constructor() { }
-
-  ngOnInit() {
-    // console.log(this.b.nom);
-  }
-
-}
-
-=======
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import Biere from '../biere';
 import { trigger, state, style, transition, animate } from '@angular/animations';
@@ -73,4 +50,3 @@ export class BiereComponent implements OnInit {
   }
 
 }
->>>>>>> master
